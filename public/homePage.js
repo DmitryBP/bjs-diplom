16e659386a6a52b4ea2f603d6d1f950b1cb92a79
Dmitry_home <79811924115@yandex.ru>
2022-03-22 08:22:07 +0300

    homePage exit1

--- public/homePage.js
+++ public/homePage.js
@@ -1,5 +1,8 @@
+
+
 const logoutButton = new LogoutButton();
 // const apiConnector = new ApiConnector();
 
-logoutButton.action = () => {console.log('test'); ApiConnector.logout()}
+// logoutButton.action = () => {ApiConnector.logout()}
+logoutButton.logoutClick = ApiConnector.logout(response => {console.log(response, 'До выхода 5 секунд'); setTimeout(() => location.reload(), 5000)})
 // logoutButton.action = () => {console.log('test'); location.reload()}
