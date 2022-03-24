2671fe92801921d32bad567b3a6abe9fa71fcf33
Dmitry_home <79811924115@yandex.ru>
2022-03-24 06:39:06 +0300

    diplom complited

--- public/homePage.js
+++ public/homePage.js
@@ -1,8 +1,93 @@
+const logoutButton = new LogoutButton(); //? Что значат 3 точки под  LogoutButton() (vs code)
+logoutButton.action = () => ApiConnector.logout(() => location.reload()); // Выход
 
+// Получение данных текущего пользователя
+ApiConnector.current((response) => {
+  if (response.success) {
+    ProfileWidget.showProfile(response.data);
+  }
+});
 
-const logoutButton = new LogoutButton();
-// const apiConnector = new ApiConnector();
+// Получение текущих курсов валюты
+const ratesBoard = new RatesBoard(); //? Почему RatesBoard() коричневый, а LogoutButton() синий? (vs code)
+ApiConnector.getStocks((response) => {
+  if (response.success) {
+    ratesBoard.fillTable(response.data);
+    setInterval(() => {
+      ratesBoard.clearTable();
+      ratesBoard.fillTable(response.data);
+    }, 60000);
+  }
+});
 
-// logoutButton.action = () => {ApiConnector.logout()}
-logoutButton.logoutClick = ApiConnector.logout(response => {console.log(response, 'До выхода 5 секунд'); setTimeout(() => location.reload(), 5000)})
-// logoutButton.action = () => {console.log('test'); location.reload()}
+// Операции с деньгами
+// Пополнение баланса
+const moneyManager = new MoneyManager();
+moneyManager.addMoneyCallback = (data) => {
+  ApiConnector.addMoney(data, (response) => {
+    if (response.success) {
+      ProfileWidget.showProfile(response.data);
+      moneyManager.setMessage(true, "Поднял бабло");
+    } else {
+      moneyManager.setMessage(false, response.error);
+    }
+  });
+};
+// Конвертирование валюты
+moneyManager.conversionMoneyCallback = (data) => {
+  ApiConnector.convertMoney(data, (response) => {
+    if (response.success) {
+      ProfileWidget.showProfile(response.data);
+      moneyManager.setMessage(true, "Сделка прошла успешно");
+    } else {
+      moneyManager.setMessage(false, response.error);
+    }
+  });
+};
+// Перевод валюты
+moneyManager.sendMoneyCallback = (data) => {
+  ApiConnector.transferMoney(data, (response) => {
+    if (response.success) {
+      ProfileWidget.showProfile(response.data);
+      moneyManager.setMessage(true, "Выполнен перевод валюты");
+    } else {
+      moneyManager.setMessage(false, response.error);
+    }
+  });
+};
+// Работа с избранным
+// Запрос начального списка избранного
+const favoritesWidget = new FavoritesWidget();
+ApiConnector.getFavorites((response) => {
+  if (response.success) {
+    favoritesWidget.clearTable();
+    favoritesWidget.fillTable(response.data);
+    moneyManager.updateUsersList(response.data);
+  }
+});
+// Добавление пользователя в список избранного
+favoritesWidget.addUserCallback = (data) => {
+  ApiConnector.addUserToFavorites(data, (response) => {
+    if (response.success) {
+      favoritesWidget.clearTable();
+      favoritesWidget.fillTable(response.data);
+      moneyManager.updateUsersList(response.data);
+      moneyManager.setMessage(true, "Пользователь добавлен");
+    } else {
+      moneyManager.setMessage(false, response.error);
+    }
+  });
+};
+// Удаление пользователя из избранного
+favoritesWidget.removeUserCallback = (data) => {
+  ApiConnector.removeUserFromFavorites(data, (response) => {
+    if (response.success) {
+      favoritesWidget.clearTable();
+      favoritesWidget.fillTable(response.data);
+      moneyManager.updateUsersList(response.data);
+      moneyManager.setMessage(true, "Пользователь удален");
+    } else {
+      moneyManager.setMessage(false, response.error);
+    }
+  });
+};
