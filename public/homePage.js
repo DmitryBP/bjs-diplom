666e606d34af48484da92c0a6cb7d1eb8059e468
Dmitry <79811924115@yandex.ru>
2024-07-11 17:04:04 +0300

    Дописал до пополнения баланса

---
+++ public/homePage.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const logoutButton = new LogoutButton();
+logoutButton.action = () => {
+  ApiConnector.logout((response) => {
+    if (response) {
+      location.reload();
+    }
+  });
+};
+
+ApiConnector.current((response) => {
+  if (response.success) {
+    ProfileWidget.showProfile(response.data);
+  }
+});
+
+const getCours = () => {
+  let ratesBoard = new RatesBoard();
+  ApiConnector.getStocks((response) => {
+    if (response.success) {
+      ratesBoard.clearTable();
+      ratesBoard.fillTable(response.data);
+    }
+  });
+};
+getCours();
+setInterval(() => {
+  getCours();
+}, 60000);
+
+const moneyManager = new MoneyManager();
+moneyManager.addMoneyCallback = (data) => {
+  ApiConnector.addMoney(data, (response) => {
+    if (response.success) {
+      ProfileWidget.showProfile(response.data);
+    }
+  });
+};
